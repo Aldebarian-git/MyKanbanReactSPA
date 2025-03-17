ee91159ddb7a1ed66d41f97603c3639067ac0820
Amdebarian-git <flavien.guillonfr@gmail.com>
2025-03-17 07:39:44 +0100

    done

--- src/assets/components/Title.jsx
+++ src/assets/components/Title.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Title = () => {
     return (
         <div className='flex justify-center items-center'>
-            <h1 className='text-4xl font-marker flex items-center whitespace-nowrap font-bold'>My Kanban's Board .<img src="../../public/images/marker.png" alt="logo" className='w-20 h-20 mb-7' /></h1>
+            <h1 className='text-4xl font-marker flex items-center whitespace-nowrap font-bold'>My Kanban's Board .<img src="./images/marker.png" alt="logo" className='w-20 h-20 mb-7' /></h1>
             
             
         </div>
